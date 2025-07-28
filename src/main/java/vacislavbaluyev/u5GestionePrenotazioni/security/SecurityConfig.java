204cc6558fef78de0a831689028e7bf442bcd536
TagBalu <baluyevslava@icloud.com>
2025-07-28 16:09:33 +0200

    Fatta la config iniziale

---
+++ src/main/java/vacislavbaluyev/u5GestionePrenotazioni/security/SecurityConfig.java
@@ -0,0 +1,23 @@
+package vacislavbaluyev.u5GestionePrenotazioni.security;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+   @Bean
+   public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+       httpSecurity.formLogin(formLogin->formLogin.disable());
+       httpSecurity.csrf(csrf->csrf.disable());
+       httpSecurity.sessionManagement(session->session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+       httpSecurity.authorizeHttpRequests(h -> h.requestMatchers("/**").permitAll());
+
+       return httpSecurity.build();
+   }
+}
